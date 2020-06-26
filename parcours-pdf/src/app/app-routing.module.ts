b243ff15509067c952bc3105bc96aa608743ac10
camara94 <ldamaro98@gmail.com>
2020-06-26 10:56:47 +0200

    Ouverture et telechargement en PDF

--- parcours-pdf/src/app/app-routing.module.ts
+++ parcours-pdf/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'header', component: HeaderComponent},
+  { path: 'footer', component: FooterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
